e4fdf244b308aeb3b65050e125c2c29ee5f2ddeb
Vikas <sharmavik1902@gmail.com>
2025-04-02 08:44:59 +0530

    first commit

--- src/streamlit_gui/defect_report_gui.py
+++ src/streamlit_gui/defect_report_gui.py
@@ -36,52 +36,38 @@ def get_defect_by_multi_tab():
     st.title("Filter Defect List")
 
     select_status = st.selectbox("Choose Status:", ["All","Reported", "Closed"])
-    st.write(select_status)
 
     eqpmnt_list = []  # Initialize to avoid NameError
-
     if select_status != "All":
         st.write(select_status)
-
         eqp_list_response = requests.get(f"{API_URL}/equipment_list/{select_status}")
-
         if eqp_list_response.status_code == 200:
             eqp_list_json = eqp_list_response.json()
             if isinstance(eqp_list_json, dict):  # Ensure it's a dictionary
                 eqpmnt_list = [item["equipment_id"] for item in eqp_list_json.get("equipment_list", [])]
         else:
             eqpmnt_list = ["Not fetched Equipment list"]
-            st.write("Eqp not fetched")
-
     select_eqp = st.selectbox("Choose Equipment", ["All"] + eqpmnt_list)
 
     part_list = []  # Initialize to avoid NameError
-
     if select_eqp != "All":
         part_list_response = requests.get(f"{API_URL}/part_list/{select_eqp}")
-
         if part_list_response.status_code == 200:
             part_list_json = part_list_response.json()
             if isinstance(part_list_json, dict):  # Ensure response is a dictionary
                 part_list = [item["part_id"] for item in part_list_json.get("part_list", [])]
         else:
             part_list = ["Parts not fetched"]
-            st.write("Parts not fetched")
-
     select_part = st.selectbox("Choose Part", ["All"] + part_list)
 
     report_list = []  # Initialize to avoid NameError
-
     if select_part != "All":
         report_list_response = requests.get(f"{API_URL}/distinct_defect/{select_part}")
-
         if report_list_response.status_code == 200:
             report_list_json = report_list_response.json()
             if isinstance(report_list_json, dict):  # Ensure response is a dictionary
                 report_list = [item["defect_description"] for item in report_list_json.get("defect_description", [])]
         else:
             report_list = ["Report not fetched"]
-            st.write("Report not fetched")
-
     select_report = st.selectbox("Choose Report", ["All"] + report_list)
 
