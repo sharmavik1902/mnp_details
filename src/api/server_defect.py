c3aa2d6c2502fda6cc61ee9f1c3ea0f951da7395
Vikas <sharmavik1902@gmail.com>
2025-04-02 23:18:01 +0530

    Update defect

--- src/api/server_defect.py
+++ src/api/server_defect.py
@@ -24,7 +24,7 @@ class DefectUpdateRequest(BaseModel):
     type_of_activity: str
     consumption: str
     spare: str
-    resolution_date: datetime
+    resolution_date: datetime.date()
     equipment_id: str
     part_id: str
     defect_description: str
