c9425bc394f1318ac4e70d83f0dbd07c64e90811
Vikas <sharmavik1902@gmail.com>
2025-04-03 11:12:42 +0530

    Update defect

--- src/streamlit_gui/defect_report_gui.py
+++ src/streamlit_gui/defect_report_gui.py
@@ -70,39 +70,40 @@ def get_defect_by_multi_tab():
         report_list = [item["defect_description"] for item in report_data.get("distinct_defect", [])]
     select_report = st.selectbox("Choose Report:", ["Select Report"] + report_list)
 
-    # Defect Update Form
-    with st.form("update_defect_form"):
-        assigned_technician = st.text_input("Technician Name",value="NA")
-        type_of_activity = st.text_input("Type of Activity",value="NA")
-        remarks = st.text_area("Additional Remarks",value="NA")
-        consumption = st.text_input("Consumables",value="NA")
-        spare = st.text_input("Spare Part Used",value="NA")
-        defect_status = st.selectbox("Defect Status:", ["Reported", "Closed", "Under Review", "Resolved", "Duplicate"])
-        downtime_hours = st.number_input("Down Time (hrs)",value=0.0)
-        resolution_date = st.date_input("Rectification Date")
+    if select_report!= "Select Report":
+        # Defect Update Form
+        with st.form("update_defect_form"):
+         assigned_technician = st.text_input("Technician Name",value="NA")
+            type_of_activity = st.text_input("Type of Activity",value="NA")
+            remarks = st.text_area("Additional Remarks",value="NA")
+            consumption = st.text_input("Consumables",value="NA")
+            spare = st.text_input("Spare Part Used",value="NA")
+            defect_status = st.selectbox("Defect Status:", ["Reported", "Closed", "Under Review", "Resolved", "Duplicate"])
+            downtime_hours = st.number_input("Down Time (hrs)",value=0.0)
+            resolution_date = st.date_input("Rectification Date")
 
-        submit = st.form_submit_button("Update Defect to DB")
+            submit = st.form_submit_button("Update Defect to DB")
 
-        if submit:
-            payload = {
-                "assigned_technician": assigned_technician,
-                "defect_status": defect_status,
-                "downtime_hours": downtime_hours,
-                "remarks": remarks,
-                "type_of_activity": type_of_activity,
-                "consumption": consumption,
-                "spare": spare,
-                "resolution_date": resolution_date.isoformat(),
-                "equipment_id": select_eqp,
-                "part_id": select_part,
-                "defect_description": select_report
-            }
+            if submit:
+                payload = {
+                    "assigned_technician": assigned_technician,
+                    "defect_status": defect_status,
+                    "downtime_hours": downtime_hours,
+                    "remarks": remarks,
+                    "type_of_activity": type_of_activity,
+                    "consumption": consumption,
+                    "spare": spare,
+                    "resolution_date": resolution_date.isoformat(),
+                    "equipment_id": select_eqp,
+                    "part_id": select_part,
+                    "defect_description": select_report
+                }
 
-            try:
-                response = requests.post(f"{API_URL}/update_defect/", json=payload, timeout=10)
-                if response.status_code == 200:
-                    st.success("Defect updated successfully!")
-                else:
-                    st.error(f"Failed to update defect: {response.status_code} - {response.text}")
-            except requests.exceptions.RequestException as e:
-                st.error(f"Error updating defect: {str(e)}")
+                try:
+                    response = requests.post(f"{API_URL}/update_defect/", json=payload, timeout=10)
+                    if response.status_code == 200:
+                        st.success("Defect updated successfully!")
+                    else:
+                        st.error(f"Failed to update defect: {response.status_code} - {response.text}")
+                except requests.exceptions.RequestException as e:
+                    st.error(f"Error updating defect: {str(e)}")
