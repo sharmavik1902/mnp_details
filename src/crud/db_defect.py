e034a8a6fdae72032434cb6f11966c33069acd2e
Vikas <sharmavik1902@gmail.com>
2025-04-06 00:38:33 +0530

    .env gitignore

--- src/crud/db_defect.py
+++ src/crud/db_defect.py
@@ -19,7 +19,7 @@ def get_db_cursor(commit=False): #Commit is used to reflect the data changes to 
         yield cursor
         print("again in cursor due to yield")
         if commit:
-            connection.commit() #curson got commitedd
+            connection.commit() #curson got commited
         cursor.close()
         connection.close()
     except Exception as err:
